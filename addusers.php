b868ccafbc519c4c167861a8c743b007323e2aef
ihasank <57311984+ihasank@users.noreply.github.com>
2020-10-14 19:24:46 +0530

    Add files via upload

---
+++ addusers.php
@@ -0,0 +1,226 @@
+<?php include('header.php'); ?>
+<div id="sidebar"><div id="sidebar-wrapper"> <!-- Sidebar with logo and menu -->
+			
+			<h1 id="sidebar-title"><a href="#">Simpla Admin</a></h1>
+		  
+			<!-- Logo (221px wide) -->
+			<a href="#"><img id="logo" src="resources/images/logo.png" alt="Simpla Admin logo" /></a>
+		  
+			<!-- Sidebar Profile links -->
+			<div id="profile-links">
+				Hello, <a href="#" title="Edit your profile">John Doe</a>, you have <a href="#messages" rel="modal" title="3 Messages">3 Messages</a><br />
+				<br />
+				<a href="#" title="View the Site">View the Site</a> | <a href="#" title="Sign Out">Sign Out</a>
+			</div>        
+			
+			<ul id="main-nav">  <!-- Accordion Menu -->
+				
+				<li>
+					<a href="http://www.google.com/" class="nav-top-item no-submenu"> <!-- Add the class "no-submenu" to menu items with no sub menu -->
+						Dashboard
+					</a>       
+				</li>
+				
+				<li> 
+					<a href="#" class="nav-top-item curent"> <!-- Add the class "current" to current menu item -->
+					Products
+					</a>
+					<ul>
+						
+						<li><a  href="products.php">Manage Products</a></li> <!-- Add class "current" to sub menu items also -->
+						<li><a href="categories.php">Manage Categories</a></li>
+						<li><a href="tags.php">Manage Tags</a></li>
+					</ul>
+				</li>
+				
+				<li>
+					<a href="#" class="nav-top-item current">
+						Users
+					</a>
+					<ul>
+						<li><a  class="current" href="addusers.php">Add User</a></li>
+						<li><a href="manageusers.php">Manage Users</a></li>
+					</ul>
+				</li>
+
+                <li>
+					<a href="#" class="nav-top-item">
+						Orders
+					</a>
+					<ul>
+						<li><a href="manageorders.php">Manage Orders</a></li>
+					</ul>
+				</li>
+				
+				
+				
+				<li>
+					<a href="#" class="nav-top-item">
+						Settings
+					</a>
+					<ul>
+						<li><a href="#">General</a></li>
+					</ul>
+				</li>      
+				
+			</ul> <!-- End #main-nav -->
+			
+			<div id="messages" style="display: none"> <!-- Messages are shown when a link with these attributes are clicked: href="#messages" rel="modal"  -->
+				
+				<h3>3 Messages</h3>
+			 
+				<p>
+					<strong>17th May 2009</strong> by Admin<br />
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue.
+					<small><a href="#" class="remove-link" title="Remove message">Remove</a></small>
+				</p>
+			 
+				<p>
+					<strong>2nd May 2009</strong> by Jane Doe<br />
+					Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
+					<small><a href="#" class="remove-link" title="Remove message">Remove</a></small>
+				</p>
+			 
+				<p>
+					<strong>25th April 2009</strong> by Admin<br />
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue.
+					<small><a href="#" class="remove-link" title="Remove message">Remove</a></small>
+				</p>
+				
+				<form action="#" method="post">
+					
+					<h4>New Message</h4>
+					
+					<fieldset>
+						<textarea class="textarea" name="textfield" cols="79" rows="5"></textarea>
+					</fieldset>
+					
+					<fieldset>
+					
+						<select name="dropdown" class="small-input">
+							<option value="option1">Send to...</option>
+							<option value="option2">Everyone</option>
+							<option value="option3">Admin</option>
+							<option value="option4">Jane Doe</option>
+						</select>
+						
+						<input class="button" type="submit" value="Send" />
+						
+					</fieldset>
+					
+				</form>
+				
+			</div> <!-- End #messages -->
+			
+		</div></div> <!-- End #sidebar -->
+		
+		<div id="main-content"> <!-- Main Content Section with everything -->
+			
+			<noscript> <!-- Show a notification if the user has disabled javascript -->
+				<div class="notification error png_bg">
+					<div>
+						Javascript is disabled or is not supported by your browser. Please <a href="http://browsehappy.com/" title="Upgrade to a better browser">upgrade</a> your browser or <a href="http://www.google.com/support/bin/answer.py?answer=23852" title="Enable Javascript in your browser">enable</a> Javascript to navigate the interface properly.
+					</div>
+				</div>
+			</noscript>
+			
+			<!-- Page Head -->
+			<h2>Welcome John</h2>
+			<p id="page-intro">What would you like to do?</p>
+			
+			<div class="clear"></div> <!-- End .clear -->
+			
+			<div class="content-box"><!-- Start Content Box -->
+				
+				<div class="content-box-header">
+					
+					<h3>Content box</h3>
+					
+					<ul class="content-box-tabs">
+						<li><a href="#tab1" >Manage</a></li> <!-- href must be unique and match the id of target div -->
+						<li><a href="#tab2" class="current">Add</a></li>
+					</ul>
+					
+					<div class="clear"></div>
+					
+				</div> <!-- End .content-box-header -->
+				
+				
+					
+					<div class="tab-content default-tab" id="tab1"> <!-- This is the target div. id must match the href of this div's tab -->
+						
+						
+						
+					</div> <!-- End #tab1 -->
+					
+					<div class="tab-content" id="tab2">
+					
+						<form action="#" method="post">
+							
+							<fieldset> <!-- Set class to "column-left" or "column-right" on fieldsets to divide the form into columns -->
+								
+								<p>
+									<label>Small form input</label>
+										<input class="text-input small-input" type="text" id="small-input" name="small-input" /> <span class="input-notification success png_bg">Successful message</span> <!-- Classes for input-notification: success, error, information, attention -->
+										<br /><small>A small description of the field</small>
+								</p>
+								
+								<p>
+									<label>Medium form input</label>
+									<input class="text-input medium-input datepicker" type="text" id="medium-input" name="medium-input" /> <span class="input-notification error png_bg">Error message</span>
+								</p>
+								
+								<p>
+									<label>Large form input</label>
+									<input class="text-input large-input" type="text" id="large-input" name="large-input" />
+								</p>
+								
+								<p>
+									<label>Checkboxes</label>
+									<input type="checkbox" name="checkbox1" /> This is a checkbox <input type="checkbox" name="checkbox2" /> And this is another checkbox
+								</p>
+								
+								<p>
+									<label>Radio buttons</label>
+									<input type="radio" name="radio1" /> This is a radio button<br />
+									<input type="radio" name="radio2" /> This is another radio button
+								</p>
+								
+								<p>
+									<label>This is a drop down list</label>              
+									<select name="dropdown" class="small-input">
+										<option value="option1">Option 1</option>
+										<option value="option2">Option 2</option>
+										<option value="option3">Option 3</option>
+										<option value="option4">Option 4</option>
+									</select> 
+								</p>
+								
+								<p>
+									<label>Textarea with WYSIWYG</label>
+									<textarea class="text-input textarea wysiwyg" id="textarea" name="textfield" cols="79" rows="15"></textarea>
+								</p>
+								
+								<p>
+									<input class="button" type="submit" value="Submit" />
+								</p>
+								
+							</fieldset>
+							
+							<div class="clear"></div><!-- End .clear -->
+							
+						</form>
+						
+					</div> <!-- End #tab2 -->        
+					
+				</div> <!-- End .content-box-content -->
+				
+			</div> <!-- End .content-box -->
+			
+			
+			<div class="clear"></div>
+			
+			
+
+			
+			<?php include('footer.php'); ?>
---
+++ manageusers.php
@@ -0,0 +1,319 @@
+<?php include('header.php'); ?>
+<div id="sidebar"><div id="sidebar-wrapper"> <!-- Sidebar with logo and menu -->
+			
+			<h1 id="sidebar-title"><a href="#">Simpla Admin</a></h1>
+		  
+			<!-- Logo (221px wide) -->
+			<a href="#"><img id="logo" src="resources/images/logo.png" alt="Simpla Admin logo" /></a>
+		  
+			<!-- Sidebar Profile links -->
+			<div id="profile-links">
+				Hello, <a href="#" title="Edit your profile">John Doe</a>, you have <a href="#messages" rel="modal" title="3 Messages">3 Messages</a><br />
+				<br />
+				<a href="#" title="View the Site">View the Site</a> | <a href="#" title="Sign Out">Sign Out</a>
+			</div>        
+			
+			<ul id="main-nav">  <!-- Accordion Menu -->
+				
+				<li>
+					<a href="http://www.google.com/" class="nav-top-item no-submenu"> <!-- Add the class "no-submenu" to menu items with no sub menu -->
+						Dashboard
+					</a>       
+				</li>
+				
+				<li> 
+					<a href="#" class="nav-top-item "> <!-- Add the class "current" to current menu item -->
+					Products
+					</a>
+					<ul>
+						
+						<li><a  href="products.php">Manage Products</a></li> <!-- Add class "current" to sub menu items also -->
+						<li><a href="categories.php">Manage Categories</a></li>
+						<li><a href="tags.php">Manage Tags</a></li>
+					</ul>
+				</li>
+				
+				<li>
+					<a href="#" class="nav-top-item current">
+						Users
+					</a>
+					<ul>
+						<li><a href="addusers.php">Add User</a></li>
+						<li><a  class="current" href="manageusers.php">Manage Users</a></li>
+					</ul>
+				</li>
+
+                <li>
+					<a href="#" class="nav-top-item">
+						Orders
+					</a>
+					<ul>
+						<li><a href="#">Manage Orders</a></li>
+					</ul>
+				</li>
+				
+				
+				
+				<li>
+					<a href="#" class="nav-top-item">
+						Settings
+					</a>
+					<ul>
+						<li><a href="#">General</a></li>
+					</ul>
+				</li>      
+				
+			</ul> <!-- End #main-nav -->
+			
+			<div id="messages" style="display: none"> <!-- Messages are shown when a link with these attributes are clicked: href="#messages" rel="modal"  -->
+				
+				<h3>3 Messages</h3>
+			 
+				<p>
+					<strong>17th May 2009</strong> by Admin<br />
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue.
+					<small><a href="#" class="remove-link" title="Remove message">Remove</a></small>
+				</p>
+			 
+				<p>
+					<strong>2nd May 2009</strong> by Jane Doe<br />
+					Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
+					<small><a href="#" class="remove-link" title="Remove message">Remove</a></small>
+				</p>
+			 
+				<p>
+					<strong>25th April 2009</strong> by Admin<br />
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue.
+					<small><a href="#" class="remove-link" title="Remove message">Remove</a></small>
+				</p>
+				
+				<form action="#" method="post">
+					
+					<h4>New Message</h4>
+					
+					<fieldset>
+						<textarea class="textarea" name="textfield" cols="79" rows="5"></textarea>
+					</fieldset>
+					
+					<fieldset>
+					
+						<select name="dropdown" class="small-input">
+							<option value="option1">Send to...</option>
+							<option value="option2">Everyone</option>
+							<option value="option3">Admin</option>
+							<option value="option4">Jane Doe</option>
+						</select>
+						
+						<input class="button" type="submit" value="Send" />
+						
+					</fieldset>
+					
+				</form>
+				
+			</div> <!-- End #messages -->
+			
+		</div></div> <!-- End #sidebar -->
+		
+		<div id="main-content"> <!-- Main Content Section with everything -->
+			
+			<noscript> <!-- Show a notification if the user has disabled javascript -->
+				<div class="notification error png_bg">
+					<div>
+						Javascript is disabled or is not supported by your browser. Please <a href="http://browsehappy.com/" title="Upgrade to a better browser">upgrade</a> your browser or <a href="http://www.google.com/support/bin/answer.py?answer=23852" title="Enable Javascript in your browser">enable</a> Javascript to navigate the interface properly.
+					</div>
+				</div>
+			</noscript>
+			
+			<!-- Page Head -->
+			<h2>Welcome John</h2>
+			<p id="page-intro">What would you like to do?</p>
+			
+			<div class="clear"></div> <!-- End .clear -->
+			
+			<div class="content-box"><!-- Start Content Box -->
+				
+				<div class="content-box-header">
+					
+					<h3>Content box</h3>
+					
+					<ul class="content-box-tabs">
+						<li><a href="#tab1" class="current">Manage</a></li> <!-- href must be unique and match the id of target div -->
+						<li><a href="#tab2" >Add</a></li>
+					</ul>
+					
+					<div class="clear"></div>
+					
+				</div> <!-- End .content-box-header -->
+				
+				
+					
+					<div class="tab-content default-tab" id="tab1"> <!-- This is the target div. id must match the href of this div's tab -->
+						
+						
+						
+					</div> <!-- End #tab1 -->
+					
+					<div class="tab-content" id="tab2">
+					
+						<form action="#" method="post">
+							
+                        <table>
+							
+							<thead>
+								<tr>
+								   <th><input class="check-all" type="checkbox" /></th>
+								   <th>Column 1</th>
+								   <th>Column 2</th>
+								   <th>Column 3</th>
+								   <th>Column 4</th>
+								   <th>Column 5</th>
+								</tr>
+								
+							</thead>
+						 
+							<tfoot>
+								<tr>
+									<td colspan="6">
+										<div class="bulk-actions align-left">
+											<select name="dropdown">
+												<option value="option1">Choose an action...</option>
+												<option value="option2">Edit</option>
+												<option value="option3">Delete</option>
+											</select>
+											<a class="button" href="#">Apply to selected</a>
+										</div>
+										
+										<div class="pagination">
+											<a href="#" title="First Page">&laquo; First</a><a href="#" title="Previous Page">&laquo; Previous</a>
+											<a href="#" class="number" title="1">1</a>
+											<a href="#" class="number" title="2">2</a>
+											<a href="#" class="number current" title="3">3</a>
+											<a href="#" class="number" title="4">4</a>
+											<a href="#" title="Next Page">Next &raquo;</a><a href="#" title="Last Page">Last &raquo;</a>
+										</div> <!-- End .pagination -->
+										<div class="clear"></div>
+									</td>
+								</tr>
+							</tfoot>
+						 
+							<tbody>
+								<tr>
+									<td><input type="checkbox" /></td>
+									<td>Lorem ipsum dolor</td>
+									<td><a href="#" title="title">Sit amet</a></td>
+									<td>Consectetur adipiscing</td>
+									<td>Donec tortor diam</td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="#" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 <a href="#" title="Edit Meta"><img src="resources/images/icons/hammer_screwdriver.png" alt="Edit Meta" /></a>
+									</td>
+								</tr>
+								
+								<tr>
+									<td><input type="checkbox" /></td>
+									<td>Lorem ipsum dolor</td>
+									<td><a href="#" title="title">Sit amet</a></td>
+									<td>Consectetur adipiscing</td>
+									<td>Donec tortor diam</td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="#" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 <a href="#" title="Edit Meta"><img src="resources/images/icons/hammer_screwdriver.png" alt="Edit Meta" /></a>
+									</td>
+								</tr>
+								
+								<tr>
+									<td><input type="checkbox" /></td>
+									<td>Lorem ipsum dolor</td>
+									<td><a href="#" title="title">Sit amet</a></td>
+									<td>Consectetur adipiscing</td>
+									<td>Donec tortor diam</td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="#" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 <a href="#" title="Edit Meta"><img src="resources/images/icons/hammer_screwdriver.png" alt="Edit Meta" /></a>
+									</td>
+								</tr>
+								
+								<tr>
+									<td><input type="checkbox" /></td>
+									<td>Lorem ipsum dolor</td>
+									<td><a href="#" title="title">Sit amet</a></td>
+									<td>Consectetur adipiscing</td>
+									<td>Donec tortor diam</td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="#" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 <a href="#" title="Edit Meta"><img src="resources/images/icons/hammer_screwdriver.png" alt="Edit Meta" /></a>
+									</td>
+								</tr>
+								
+								<tr>
+									<td><input type="checkbox" /></td>
+									<td>Lorem ipsum dolor</td>
+									<td><a href="#" title="title">Sit amet</a></td>
+									<td>Consectetur adipiscing</td>
+									<td>Donec tortor diam</td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="#" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 <a href="#" title="Edit Meta"><img src="resources/images/icons/hammer_screwdriver.png" alt="Edit Meta" /></a>
+									</td>
+								</tr>
+								
+								<tr>
+									<td><input type="checkbox" /></td>
+									<td>Lorem ipsum dolor</td>
+									<td><a href="#" title="title">Sit amet</a></td>
+									<td>Consectetur adipiscing</td>
+									<td>Donec tortor diam</td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="#" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 <a href="#" title="Edit Meta"><img src="resources/images/icons/hammer_screwdriver.png" alt="Edit Meta" /></a>
+									</td>
+								</tr>
+								
+								<tr>
+									<td><input type="checkbox" /></td>
+									<td>Lorem ipsum dolor</td>
+									<td><a href="#" title="title">Sit amet</a></td>
+									<td>Consectetur adipiscing</td>
+									<td>Donec tortor diam</td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="#" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 <a href="#" title="Edit Meta"><img src="resources/images/icons/hammer_screwdriver.png" alt="Edit Meta" /></a>
+									</td>
+								</tr>
+								
+								<tr>
+									<td><input type="checkbox" /></td>
+									<td>Lorem ipsum dolor</td>
+									<td><a href="#" title="title">Sit amet</a></td>
+									<td>Consectetur adipiscing</td>
+									<td>Donec tortor diam</td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="#" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 <a href="#" title="Edit Meta"><img src="resources/images/icons/hammer_screwdriver.png" alt="Edit Meta" /></a>
+									</td>
+								</tr>
+							</tbody>
+							
+						</table>
+			<div class="clear"></div>
+			
+			
+
+			
+			<?php include('footer.php'); ?>
